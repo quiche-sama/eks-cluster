30de9d1d3e299feb2fdbde32dc21341d208b5cff
Pierre F <41551115+quiche-sama@users.noreply.github.com>
2022-07-25 16:25:46 +0200

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -24,7 +24,7 @@ module "eks" {
       instance_type                 = "t2.xlarge"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
+      asg_desired_capacity          = 5
     },
   ]
 }
